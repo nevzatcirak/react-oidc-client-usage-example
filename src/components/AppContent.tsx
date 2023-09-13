e2d65a82b9aa9a01b2b71e69982c79d114d5a507
nevzatcirak <nevzatcirak17@gmail.com>
2023-09-13 21:42:08 +0200

    Redundant axios is removed

--- src/components/AppContent.tsx
+++ src/components/AppContent.tsx
@@ -2,10 +2,8 @@ import * as React from "react";
 
 import AuthContent from "./AuthContent";
 import Buttons from "./Buttons";
-import { useAuthentication } from "@nevzatcirak/react-oidc-client";
+import {useAuthentication} from "@nevzatcirak/react-oidc-client";
 // import { useAuthentication } from "../openid";
-
-import axios from '../axios-config';
 
 export default function AppContent() {
   const { oidcUser, logout, renewToken } = useAuthentication();
